9828ae1e2b3c21b6f5648c34c5667a8cc5f9b5c0
RayOnoda <2301149@s.asojuku.ac.jp>
2025-06-04 12:11:18 +0900

    loginデザイン追加
    ログイン画面デザイン追加

--- login.php
+++ login.php
@@ -4,18 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/login.css">
 </head>
 <body>
-    <div class="login-1">
+    <div class="back-clor">
         <h1>ログイン</h1>
-        <div class="login-form">
-        <form action="check.php" method="post">
-                <p>メールアドレス</p>
-                <input class="text" type="text" name="username" value="santoukin">
-                <p>パスワード</p>
-                <input class="text" type="password" name="password" value="0000"><br><br>
-                <input class="button" type="submit" value="ログイン">
-            </form>
-        </div>
+        <form class="login-form" action="check.php" method="post">
+            <input type="text" name="username" placeholder="ユーザー名" required>
+            <input type="password" name="password" placeholder="パスワード" required>
+            <input class="button" type="submit" value="ログイン">
+        </form>
+    </div>
 </body>
 </html>
