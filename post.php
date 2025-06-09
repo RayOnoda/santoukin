1c336792f92d66bf169076383f461ced1d7556fc
HARUTOO-aso <2401331@s.asojuku.ac.jp>
2025-06-10 08:11:07 +0900

    テスト

--- post.php
+++ post.php
@@ -10,9 +10,10 @@ if (trim($comment) === '') {
     header("Location: form.php");
     exit;
 }
+$user_id = $_SESSION['user_id'];
 
 $stmt = $pdo->prepare("INSERT INTO comment (user_id, content, created_at) VALUES (?,?,?)");
-$stmt->execute([$_SESSION['username'], $comment, $time]);
+$stmt->execute([$user_id, $comment, $time]);
 
 // $entry = "$time\t$name\t$comment\n";
 // file_put_contents('comments.txt', $entry, FILE_APPEND);
