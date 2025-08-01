07fb69b6b302e31421aa353b600f12d5f73d8352
HRUTOO-aso <2401331@s.asojuku.ac.jp>
2025-08-01 20:33:37 +0900

    詳細変更

--- check.php
+++ check.php
@@ -9,14 +9,14 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $stmt = $pdo->prepare("SELECT * FROM user WHERE username = ?");
     $stmt->execute([$username]);
     $user = $stmt->fetch();
-
-    // 平文のパスワードを直接比較
+    
     if ($user && $password === $user['password']) {
         $_SESSION['user_id'] = $user['id'];
         $_SESSION['username'] = $user['username'];
+
         header("Location: form.php");
         exit;
     } else {
-        echo "ログインに失敗しました。<a href='form.php'>戻る</a>";
+        echo "ログインに失敗しました。<a href='login.php'>戻る</a>";
     }
 }
--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 $host = 'localhost';          
 $dbname = 'santoukin';     
 $user = 'root';    
-$password = ''; // ←DBのパスワード（XAMPPなら空文字""のことが多い）
+$password = 'root'; // ←DBのパスワード（XAMPPなら空文字""のことが多い）
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
